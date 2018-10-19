f66cf80c3189df90bc5ba4dc78aca682ae7dd691
njaramillo-ng <njaramillo@nemorisgames.com>
2018-10-19 16:58:09 -0300

    Mapa con scroll, zoom y gps funcionando

---
+++ Assets/PuntoRuta.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TweenColor))]
+public class PuntoRuta : MonoBehaviour {
+	public enum Tipo{
+		Principal,
+		Secundario
+	}
+
+	public Tipo tipo = Tipo.Secundario;
+	public int indicePunto;
+	public bool puntoVisitado = false;
+	private Ruta ruta;
+	private TweenColor tweenColor;
+
+	void Awake(){
+		tweenColor = GetComponent<TweenColor>();
+	}
+
+	public void InitPunto(int i, Ruta r){
+		ruta = r;
+		indicePunto = i;
+		puntoVisitado = PlayerPrefs.GetInt("visitadoPunto"+indicePunto) == 1;
+		if(puntoVisitado)
+			PuntoVisitado();
+	}
+
+	public void PuntoVisitado(){
+		if(!puntoVisitado){
+			PlayerPrefs.SetInt("visitadoPunto"+indicePunto,1);
+			puntoVisitado = true;
+		}
+		//marcar sprite como visitado
+	}
+
+	public void EnterPoint(){
+		tweenColor.PlayForward();
+	}
+
+	public void ExitPoint(){
+		tweenColor.PlayReverse();
+	}
+}
---
+++ Assets/ZoomCamara.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCamara : MonoBehaviour {
+	Camera camera;
+	public float zoomFactor = 20f;
+	public float minZoom = 200f;
+	public float maxZoom = 400f;
+
+	// Use this for initialization
+	void Awake () {
+		camera = GetComponent<Camera>();
+	}
+	
+	public void ZoomIn(){
+		Zoom(true);
+	}
+
+	public void ZoomOut(){
+		Zoom(false);
+	}
+
+	void Zoom(bool b){
+		float zoom = camera.orthographicSize + (b ? -1 : 1 )*zoomFactor;
+		camera.orthographicSize = Mathf.Clamp(zoom,minZoom,maxZoom);
+	}
+}
